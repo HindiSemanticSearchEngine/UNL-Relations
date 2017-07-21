185ce86ef687a7274d5e470f01e682ce114d65f4
Siddharth Singh <siddharthsinghraghuptra@gmail.com>
2017-07-22 02:02:12 +0530

    Update dur.py

--- dur.py
+++ dur.py
@@ -1,14 +1,15 @@
 def duration(w1,w2):
-    if w1['pos_tag']=='VBD' and w2['pos_tag']=='CD':
+    if w1['pos_tag']=='RB' and w2['pos_tag']=='INTF':
         return True
-    elif w1['pos_tag']=='VBD' and w2['pos_tag']=='JJ':
+    elif w1['pos_tag']=='VAUX' and w2['pos_tag']=='QNUM':
         return True
-    elif w1['pos_tag']=='VBD' and w2['pos_tag']=='CD':
+    elif w1['pos_tag']=='RB' and w2['pos_tag']=='QNUM':
         return True
-    elif w1['pos_tag']=='VBG' and w2['pos_tag']=='VBG':
+    elif w1['pos_tag']=='NNPC' and w2['pos_tag']=='INTF':
         return True  
-    elif w1['pos_tag']=='VBD' and w2['pos_tag'] = 'NNS':
-	return True	
+    elif w1['pos_tag']=='NN' and w2['pos_tag'] == 'QF':
+	return True
+    elif w1['pos_tag']=='NNP' and w2['pos_tag'] == 'QF':
     else:
         return False
     
