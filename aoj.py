d809aeb9f495b7f6ee1a2652bde1d3a8eae0a297
Siddharth Singh <siddharthsinghraghuptra@gmail.com>
2017-07-22 02:03:15 +0530

    Update aoj.py

--- aoj.py
+++ aoj.py
@@ -1,11 +1,11 @@
 aoj(self,word1,word2):
-  if w1['pos_tag']==NNP and w2['pos_tag']==PRP:
+  if w1['pos_tag']=='NNP' and w2['pos_tag']=='PRP':
     return True
-  elif w1['pos_tag']==PRP and w2['pos_tag']==NN:
+  elif w1['pos_tag']=='PRP' and w2['pos_tag']=='NN':
     return True
-  elif w1['pos_tag']==NN and w2['pos_tag']==PRP:
+  elif w1['pos_tag']=='NN' and w2['pos_tag']=='PRP':
     return True
-  elif w1['pos_tag'] == NN and w2['pos_tag']==JJ:
+  elif w1['pos_tag'] == 'NN' and w2['pos_tag']=='JJ':
     return True
   else:
     return False
