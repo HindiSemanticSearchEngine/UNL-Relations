55c36d64a65f8242419215bed34fbcde3cba1c3d
Ritvik Jain <ritvikjain2@gmail.com>
2017-07-24 22:20:37 +0530

    Update and.py

--- and.py
+++ and.py
@@ -1,12 +1,12 @@
-def and_relation(self, w1, w2):
+def and_relation(self, w1, w2, w3):
 
-    if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'NN':
+    if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'CC' and  w3['pos_tag'] == 'NN':
         return True
-    elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'NNP':
+    elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NNP':
         return True
-    elif w1['pos_tag'] == 'RB' and w2['pos_tag'] == 'RB':
+    elif w1['pos_tag'] == 'RB' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'RB':
         return True
-    elif w1['pos_tag'] == 'JJ' and w2['pos_tag'] == 'JJ':
+    elif w1['pos_tag'] == 'JJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
         return True
     else:
         return False
