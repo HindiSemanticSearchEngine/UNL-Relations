c0d3ce860b3be33979729fb780c0373c35e61d60
evilashish <theevil.ashish@gmail.com>
2017-07-29 22:25:58 +0530

    Add files via upload

---
+++ obj.py
@@ -0,0 +1,21 @@
+def obj(w1,w2):
+	if w1['pos_tag']=='NN' and w2['pos_tag']=='VM:
+		return True
+
+	elif w1['pos_tag']=='VM' and w2['pos_tag']=='NN':
+		return True
+
+	elif w1['pos_tag']=='NNP' and w2['pos_tag']=='VM':
+                return True
+
+        elif w1['pos_tag']=='VM' and w2['pos_tag']=='NNP':
+                return True
+
+        elif w1['pos_tag']=='VAUX' and w2['pos_tag']=='NNP':
+                return True
+
+        elif w1['pos_tag']=='NNP' and w2['pos_tag']=='VAUX':
+            
+
+	else:
+		return False
